80b31e6149df58ec2b1cd5f4310ab3e0a4235de3
she11fish <trolltrolltrolllollollol@outlook.com>
2023-04-02 08:22:07 -0400

    fixing cors issues

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors({
     origin: ["https://frontend-git-main-school-saver.vercel.app", "https://ubiquitous-bonbon-316cbf.netlify.app"],
     methods: ["GET", "POST", "PATCH", "DELETE"]
 }));
+app.options('*', cors({
+    origin: ["https://frontend-git-main-school-saver.vercel.app", "https://ubiquitous-bonbon-316cbf.netlify.app"],
+    methods: ["GET", "POST", "PATCH", "DELETE"]
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
