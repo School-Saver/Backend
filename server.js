81b7a8fb8e79ec66262167efe581938e1c053d8f
she11fish <trolltrolltrolllollollol@outlook.com>
2023-04-01 21:50:11 -0400

    fixing cors issue

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://main--ubiquitous-bonbon-316cbf.netlify.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 
