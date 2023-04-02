da77b652a98171f6630eeb42ed0646eaef5b8cb8
she11fish <trolltrolltrolllollollol@outlook.com>
2023-04-02 06:47:48 -0400

    fixed server issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://main--ubiquitous-bonbon-316cbf.netlify.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://ubiquitous-bonbon-316cbf.netlify.app/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
