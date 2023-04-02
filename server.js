cf0b8393aeebf1a81446554c91b5990f3affe621
she11fish <trolltrolltrolllollollol@outlook.com>
2023-04-02 10:51:45 -0400

    final adjustments

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from 'cors'
 import apiRouter from './routes/index.js'
+import { config } from "dotenv"
+config()
 
 const app = express();
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: ["https://frontend-git-main-school-saver.vercel.app", "https://ubiquitous-bonbon-316cbf.netlify.app"],
+    origin: process.env.URL,
     methods: ["GET", "POST", "PATCH", "DELETE"]
 }));
 app.options('*', cors({
-    origin: ["https://frontend-git-main-school-saver.vercel.app", "https://ubiquitous-bonbon-316cbf.netlify.app"],
+    origin: process.env.URL,
     methods: ["GET", "POST", "PATCH", "DELETE"]
 }))
 app.use(express.json());
