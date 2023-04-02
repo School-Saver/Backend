4cb78f092e91f19bfe27a51206d3186bb4ed2c7f
she11fish <trolltrolltrolllollollol@outlook.com>
2023-04-02 07:27:45 -0400

    fixed server issue

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ import apiRouter from './routes/index.js'
 const app = express();
 const port = process.env.PORT || 5000
 
-app.use(cors());
+app.use(cors({
+    origin: "https://ubiquitous-bonbon-316cbf.netlify.app/",
+    methods: ["GET", "POST", "PATCH", "DELETE"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://ubiquitous-bonbon-316cbf.netlify.app/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
